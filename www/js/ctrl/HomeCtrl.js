0971f90b28b57ef138d5cfeafda20a57a5cf8368
Corbyn Salisbury <corbynsal@utexas.edu>
2013-09-03 11:18:37 -0500

    Number of favorite queries is displayed correctly on Home page

--- www/js/ctrl/HomeCtrl.js
+++ www/js/ctrl/HomeCtrl.js
@@ -1,11 +1,11 @@
 angular.module('clearConcert')
-.controller('HomeCtrl',['$scope','$location',
-function($scope, $location){
+.controller('HomeCtrl',['$scope','$location', 'Favorites', 'settings', 
+                        function($scope, $location, Favorites, settings){
 	$scope.search={
 		text:""
 	};
-
 	
+	$scope.queryFavsLength = Favorites.getFavsByType(settings.repository, favoriteTypes.QUERY).length;
 
 	$scope.go = function(target) {
 		$scope.$hidePanel();
